893f42b3c3ff9e8708372e3d8fcc5c915b49de3d
kongli26489 <kongli26489@hundsun.com>
2020-07-16 10:45:42 +0800

    脚本

--- redis/src/main/java/redis/Test.java
+++ redis/src/main/java/redis/Test.java
@@ -21,16 +21,38 @@ public class Test {
             String[] split = args[0].split(",");
             //1-asset
             jedis.set("asset", split[0]);
+            //1-assetRollback
+            jedis.set("assetRollback", split[1]);
             //2-bank
-            jedis.set("bank", split[1]);
+            jedis.set("bank", split[2]);
             //3-verify
-            jedis.set("verify", split[2]);
+            jedis.set("verify", split[3]);
+            if (split.length == 8) {
+                //1-payOutAsset
+                jedis.set("payOutAsset", split[4]);
+                //1-payOutAssetRollback
+                jedis.set("payOutAssetRollback", split[5]);
+                //2-payOutBank
+                jedis.set("payOutBank", split[6]);
+                //3-payOutVerify
+                jedis.set("payOutVerify", split[7]);
+            }
             String asset = jedis.get("asset");
+            String assetRollback = jedis.get("assetRollback");
             String bank = jedis.get("bank");
             String verify = jedis.get("verify");
+            String payOutAsset = jedis.get("payOutAsset");
+            String payOutAssetRollback = jedis.get("payOutAssetRollback");
+            String payOutBank = jedis.get("payOutBank");
+            String payOutVerify = jedis.get("payOutVerify");
             System.out.println("asset:" + asset);
+            System.out.println("assetRollback:" + assetRollback);
             System.out.println("bank:" + bank);
             System.out.println("verify:" + verify);
+            System.out.println("payOutAsset:" + payOutAsset);
+            System.out.println("payOutAssetRollback:" + payOutAssetRollback);
+            System.out.println("payOutBank:" + payOutBank);
+            System.out.println("payOutVerify:" + payOutVerify);
         } catch (Exception e) {
             e.printStackTrace();
         }
